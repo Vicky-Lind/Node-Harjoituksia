b281d451e1dbe128a63a60602aaf75123568d0b3
Vicky-Lind <112470720+Vicky-Lind@users.noreply.github.com>
2023-09-01 15:50:35 +0300

    01.09.2023

--- app.js
+++ app.js
@@ -1,34 +1,66 @@
-// WEB SERVER FOR ELECTRICITY USAGE PLANNING: WEB PAGES AND AN API
-// ---------------------------------------------------------------
+// WEB-PALVELIN SÄHKÖN HINNAN SEURANTAAN JA ENNUSTAMISEEN
+// ======================================================
 
-// LIBRARIES AND MODULES
+// KIRJASTOJEN LATAUKSET
 // ---------------------
 
-// Use Express as web engine
+// Ladataan tarvittavat Express kirjastot
 const express = require('express')
-// Use Express Handlebars as template engine
 const { engine } = require('express-handlebars')
 
-// EXPRESS APPLICATION SETTINGS
-// ----------------------------
+// EXPRESS-SOVELLUKSEN ASETUKSET
+// -----------------------------
 
-// Create the server
+// Luodaan Express-sovellus ja määritellään kuunneltava TCP-portti ympäristömuuttujasta tai 8080
 const app = express()
 const PORT = process.env.PORT || 8080
 
-// Set folder paths
-// public is for assets
+// Määritellään resurssien, kuten css-tiedostojen kansioksi public
 app.use(express.static('public'))
-// views is for pages
-app.set('views', './views')
 
-// Engine settings
+// Määritellään mallineiden käyttöasetukset
 app.engine('handlebars', engine())
 app.set('view engine', 'handlebars')
 
-// URL ROUTES
-// ----------
+// Määritellään hakemisto, josta näkymät (sivut) löytyvät
+app.set('views', './views')
 
-// START THE LISTENER
+// REITTIEN MÄÄRITYKSET
+// --------------------
+
+// Kotisivun reitti ja dynaaminen data
+app.get('/', (req, res) => {
+  // Testidataa dynaamisen sivun testaamiseksi
+  // Handlebars needs a key to show data on a page, json is a good way to send it
+  let homePageData = {
+    'price': 32.25,
+    'wind': 4,
+    'temperature': 21
+    }
+  // Render index.handlebars and send dynamic data to the page
+  res.render('index', homePageData)
+})
+
+// Tuntihintasivun reitti ja dynaaminen data
+app.get('/hourly', (req, res) => {
+  // Testidataa dynaamisen sivun testaamiseksi
+  // Data will be represented in a table. To loop all rows we need a key for table and for column data
+  let hourlyPageData = {
+    'tableData': [
+      { 'hour': 8, 'price': 31.22 },
+      { 'hour': 9, 'price': 32.55 },
+      { 'hour': 10, 'price': 31.44 },
+      { 'hour': 11, 'price': 34.11 }
+    ]
+  }
+
+  res.render('hourly', hourlyPageData)
+})
+
+app.get('/test-homepage', (req, res) => {
+    res.render('testHomepage')
+})
+
+// PALVELIMEN KÄYNNISTYS
 app.listen(PORT)
-console.log('Server started and it will listen TCP port', PORT)
+console.log(`Palvelin kuuntelee porttia ${PORT}`)
