6f09bb082abff946ddfe5cf59031ebebd1cf5d09
Vicky-Lind <112470720+Vicky-Lind@users.noreply.github.com>
2023-09-26 12:25:27 +0300

    26.09.2023

--- app.js
+++ app.js
@@ -30,37 +30,58 @@ app.set('views', './views')
 
 // Kotisivun reitti ja dynaaminen data
 app.get('/', (req, res) => {
-  // Testidataa dynaamisen sivun testaamiseksi
-  // Handlebars needs a key to show data on a page, json is a good way to send it
-  let homePageData = {
-    'price': 32.25,
-    'wind': 4,
-    'temperature': 21
+    // Testidataa dynaamisen sivun testaamiseksi
+    // Handlebars needs a key to show data on a page, json is a good way to send it
+    let homePageData = {
+        'price': 32.25,
+        'wind': 4,
+        'temperature': 21
     }
-  // Render index.handlebars and send dynamic data to the page
-  res.render('index', homePageData)
+    // Render index.handlebars and send dynamic data to the page
+    res.render('index', homePageData)
 })
 
 // Tuntihintasivun reitti ja dynaaminen data
-app.get('/hourly', (req, res) => {
-  // Testidataa dynaamisen sivun testaamiseksi
-  // Data will be represented in a table. To loop all rows we need a key for table and for column data
-  let hourlyPageData = {
-    'tableData': [
-      { 'hour': 8, 'price': 31.22 },
-      { 'hour': 9, 'price': 32.55 },
-      { 'hour': 10, 'price': 31.44 },
-      { 'hour': 11, 'price': 34.11 }
-    ]
-  }
+app.get('/spot-prices', (req, res) => {
+    // Testidataa dynaamisen sivun testaamiseksi
+    // Data will be represented in a table. To loop all rows we need a key for table and for column data
+    let hourlyPageData = {
+        'tableData': [
+            { 'hour': 8, 'price': 31.22 },
+            { 'hour': 9, 'price': 32.55 },
+            { 'hour': 10, 'price': 31.44 },
+            { 'hour': 11, 'price': 34.11 }
+        ]
+    }
 
-  res.render('hourly', hourlyPageData)
+    res.render('spot_prices', hourlyPageData)
 })
 
 app.get('/test-homepage', (req, res) => {
-    res.render('testHomepage')
+    res.render('test_homepage')
+})
+
+app.get('/test-homepage-mobile', (req, res) => {
+    res.render('test_homepage_mobile')
 })
 
 // PALVELIMEN KÄYNNISTYS
 app.listen(PORT)
 console.log(`Palvelin kuuntelee porttia ${PORT}`)
+
+// Create cron to output "hello" every other second in console
+// const cron = require('node-cron')
+// const job = cron.schedule('*/2 * * * * *', () => {
+//     console.log('hello')
+// })
+// var CronJob = require('cron').CronJob;
+// var job = new CronJob(
+//     '* * * * * *',
+//     function() {
+//         console.log('You will see this message every second');
+//     },
+//     null,
+//     true,
+//     'America/Los_Angeles'
+// );
+// job.start() - See note below when to use this
---
+++ views/api_test.js
@@ -0,0 +1,12 @@
+import fetch from 'node-fetch';
+
+const PRICE_ENDPOINT = 'https://api.porssisahko.net/v1/price.json';
+
+const dateAndTimeNow = new Date();
+const date = dateAndTimeNow.toISOString().split('T')[0];
+const hour = dateAndTimeNow.getHours();
+
+const response = await fetch(`${PRICE_ENDPOINT}?date=${date}&hour=${hour}`);
+const { price } = await response.json();
+
+console.log(`Hinta nyt on ${price}`);
